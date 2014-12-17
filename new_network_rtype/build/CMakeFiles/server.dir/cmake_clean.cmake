3cd76f23db595ce86fcd2c4e8d3fea1cf935e841
pinon <pinon_a@epitech.eu>
2014-12-17 13:08:57 +0100

    server network

---
+++ new_network_rtype/build/CMakeFiles/server.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/server.dir/src/server.cpp.o"
+  "CMakeFiles/server.dir/src/Network/Service.cpp.o"
+  "CMakeFiles/server.dir/src/Network/TcpSocket.cpp.o"
+  "CMakeFiles/server.dir/src/Network/Acceptor.cpp.o"
+  "CMakeFiles/server.dir/src/Network/UdpSocket.cpp.o"
+  "CMakeFiles/server.dir/src/Network/Timer.cpp.o"
+  "CMakeFiles/server.dir/src/Util/Time.cpp.o"
+  "CMakeFiles/server.dir/src/Util/Buffer.cpp.o"
+  "../server.pdb"
+  "../server"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/server.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
