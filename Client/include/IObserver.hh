451d12d13567b2bc1322620e22cbb38be0ebf122
bichon_b <bichon_b@epitech.eu>
2014-12-17 17:44:30 +0100

    ajout de .hh

---
+++ Client/include/IObserver.hh
@@ -0,0 +1,22 @@
+//
+//  IObserver.h
+//  TestSFML
+//
+//  Created by Charles Fournier on 02/12/2014.
+//  Copyright (c) 2014 Charles Fournier. All rights reserved.
+//
+
+#ifndef TestSFML_IObserver_h
+# define TestSFML_IObserver_h
+
+# include "MenuEnum.hh"
+
+class IObserver {
+public:
+    virtual void update(eButton idCallback) = 0;
+
+ public:
+    virtual ~IObserver() {}
+};
+
+#endif
---
+++ Client/include/MenuController.hh
@@ -0,0 +1,34 @@
+//
+//  MenuController.h
+//  TestSFML
+//
+//  Created by Charles Fournier on 02/12/2014.
+//  Copyright (c) 2014 Charles Fournier. All rights reserved.
+//
+
+#ifndef __TestSFML__MenuController__
+#define __TestSFML__MenuController__
+
+#include "IMenuListener.hh"
+#include "MenuEnum.hh"
+#include "IObserver.hh"
+#include "MenuView.hh"
+
+class MenuController : public IObserver
+{
+
+public:
+    MenuController(MenuView &view);
+    virtual ~MenuController();
+    void update(eButton idCallback);
+    void manageUpdate(eButton idCallback);
+
+   void setMenuListener(IMenuListener *);
+
+private:
+    MenuView _view;
+    IMenuListener *_listen;
+};
+
+
+#endif /* defined(__TestSFML__MenuController__) */
