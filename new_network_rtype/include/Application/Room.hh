0fae65630ec43596febb812e97ec2bf3a89de373
guerot_a <guerot_a@epitech.net>
2014-12-18 21:08:08 +0100

    Gestion du temps game

--- new_network_rtype/include/Application/Room.hh
+++ new_network_rtype/include/Application/Room.hh
@@ -2,32 +2,36 @@
 #define APPLICATION_ROOM
 
 #include <list>
+#include <chrono>
 #include "Util/Thread.hpp"
 #include "Game/CoreEventReceiver.hh"
 #include "Game/EntityEventReceiver.hh"
 #include "Game/Core.hh"
+#include "Network/TimerObserver.hh"
+#include "Network/Timer.hh"
 
 namespace Application
 {
   class Server;
   class ClientRoom;
 
-  class Room : public Util::Thread<void>,
-	       public Game::CoreEventReceiver,
-	       public Game::EntityEventReceiver
+  class Room : public Game::CoreEventReceiver,
+	       public Game::EntityEventReceiver,
+	       public Network::TimerObserver
   {
-    static const unsigned int	updatePeriod = 20;
+    typedef std::chrono::duration<int, std::milli> duration_milli;
 
   private:
-    Server&			_server;
-    std::list<ClientRoom*>	_rooms;
-    Game::Core			_game;
+    Server&					_server;
+    std::list<ClientRoom*>			_rooms;
+    Game::Core					_game;
+    std::chrono::system_clock::duration		_time;
 
   public:
     Room(Server& server);
     ~Room();
 
-    int	run();
+    void	onTimeout(Network::Timer& timer);
 
     void	receive(const Game::Entity& entity, const Game::EntityEvent::Move& event);
     void	receive(const Game::Entity& entity, const Game::EntityEvent::Life& event);
