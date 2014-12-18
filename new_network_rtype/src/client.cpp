f78a9969465c3d80f500cd127a89f6c993fd89cb
pinon <pinon_a@epitech.eu>
2014-12-18 21:24:20 +0100

    protocol tcp merged

--- new_network_rtype/src/client.cpp
+++ new_network_rtype/src/client.cpp
@@ -4,14 +4,16 @@
 #include "UdpSocket.hh"
 #include "TcpSocketObserver.hh"
 #include "UdpSocketObserver.hh"
+#include "Protocole.hh"
 
 using namespace Network;
 
 class Client : public TcpSocketObserver
 {
 private:
-  Service & _service;
-  TcpSocket _socketTCP;
+  Service &	_service;
+  TcpSocket	_socketTCP;
+  ProtocoleTcp	_protocole;
 
 public:
   Client(Service & service) : _service(service), _socketTCP(3278, "127.0.0.1")
@@ -19,6 +21,20 @@ public:
     _socketTCP.setObserver(this);
     _service.addWriteTcp(_socketTCP);
     _service.addReadTcp(_socketTCP);
+
+    Network::Header header;
+    Network::Magic magic;
+
+    header.type = Network::T_MAGIC;
+    header.data_size = sizeof(Network::Magic);
+
+    Network::packet * packet = _protocole.pack(&header);
+    Network::packet * m_packet = _protocole.pack(&magic);
+
+    _socketTCP.sendData(packet->getData(), packet->getSize());
+    _socketTCP.sendData(m_packet->getData(), m_packet->getSize());
+    delete packet;
+    delete m_packet;
   }
 
   ~Client()
