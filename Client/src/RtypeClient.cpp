8dd2b826e8062604d46d4036db79927af15a9b45
Emmanuel BONIN <emmanuel.bonin@epitech.eu>
2014-12-21 02:12:12 +0100

    .

--- Client/src/RtypeClient.cpp
+++ Client/src/RtypeClient.cpp
@@ -79,12 +79,27 @@ void	RtypeClient::onKeyEvent(RtypeEvent::eKeyEvent event)
   _tcpConnection->write(&event, sizeof(event));
 }
 
-void	RtypeClient::onMagic(RtypeProtocol::Magic)
+void	RtypeClient::onMagicBadVersion(RtypeProtocol::Magic)
 {
   std::cout << __FUNCTION__ << std::endl;
 }
 
-void	RtypeClient::onConnection()
+void	RtypeClient::onMagicAccept(RtypeProtocol::Magic)
+{
+  std::cout << __FUNCTION__ << std::endl;
+}
+
+void	RtypeClient::onConnectionAlreadyConnected()
+{
+  std::cout << __FUNCTION__ << std::endl;
+}
+
+void	RtypeClient::onConnectionInternalError()
+{
+  std::cout << __FUNCTION__ << std::endl;
+}
+
+void	RtypeClient::onConnectionOk()
 {
   std::cout << __FUNCTION__ << std::endl;
 }
@@ -103,7 +118,47 @@ void	RtypeClient::onRoomInfo(RtypeProtocol::Room room)
   //   _menuController->deleteFromRoomList(room);
 }
 
-void	RtypeClient::onPingPong(RtypeProtocol::PingPong)
+void	RtypeClient::onRoomCreateAlreadyExist(RtypeProtocol::Room)
+{
+  std::cout << __FUNCTION__ << std::endl;
+}
+
+void	RtypeClient::onRoomCreateInternalError(RtypeProtocol::Room)
+{
+  std::cout << __FUNCTION__ << std::endl;
+}
+
+void	RtypeClient::onRoomCreateOk(RtypeProtocol::Room)
+{
+  std::cout << __FUNCTION__ << std::endl;
+}
+
+void	RtypeClient::onRoomJoinNotFound(RtypeProtocol::Room)
+{
+  std::cout << __FUNCTION__ << std::endl;
+}
+
+void	RtypeClient::onRoomJoinIsFull(RtypeProtocol::Room)
+{
+  std::cout << __FUNCTION__ << std::endl;
+}
+
+void	RtypeClient::onRoomJoinBadPswd(RtypeProtocol::Room)
+{
+  std::cout << __FUNCTION__ << std::endl;
+}
+
+void	RtypeClient::onRoomJoinOk(RtypeProtocol::Room)
+{
+  std::cout << __FUNCTION__ << std::endl;
+}
+
+void	RtypeClient::onRoomExitOk(RtypeProtocol::Room)
+{
+  std::cout << __FUNCTION__ << std::endl;
+}
+
+void	RtypeClient::onPing(RtypeProtocol::PingPong)
 {
   std::cout << __FUNCTION__ << std::endl;
 }
