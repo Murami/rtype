880d3dc9c07643980382a93c5916c95570cd630f
senesi_t <senesi.theo@gmail.com>
2014-12-21 15:11:47 +0100

    test size

--- new_network_rtype/src/main.cpp
+++ new_network_rtype/src/main.cpp
@@ -2,8 +2,22 @@
 #include "Service.hh"
 #include "Server.hh"
 
+#include "RtypeProtocol.hh"
+#include <iostream>
+
 int main()
 {
+	RtypeProtocol::Room	room;
+	std::cout << "id : " << sizeof(room.id) << std::endl;
+	std::cout << "alive : " << sizeof(room.alive) << std::endl;
+	std::cout << "connected_user : " << sizeof(room.connected_users) << std::endl;
+	std::cout << "nb_connected_users : " << sizeof(room.nb_connected_users) << std::endl;
+	std::cout << "ipassword_flagd : " << sizeof(room.password_flag) << std::endl;
+	std::cout << "pass_md5 : " << sizeof(room.pass_md5) << std::endl;
+	std::cout << "room_name : " << sizeof(room.room_name) << std::endl;
+	std::cout << "ready_users : " << sizeof(room.ready_users) << std::endl;
+	std::cout << "locked : " << sizeof(room.locked) << std::endl;
+	std::cout << "users_id : " << sizeof(room.users_id) << std::endl;
   Network::Service	service;
   Application::Server	server(service);
 
