d5b3383d710c2caa447a450b3ac2a2c4fb9c5d04
guerot_a <guerot_a@epitech.net>
2014-12-21 15:54:04 +0100

    .

--- new_network_rtype/src/main.cpp
+++ new_network_rtype/src/main.cpp
@@ -3,21 +3,22 @@
 #include "Server.hh"
 
 #include "RtypeProtocol.hh"
+#include <cstddef>
 #include <iostream>
 
 int main()
 {
-	RtypeProtocol::Room	room;
-	std::cout << "id : " << offsetof(RtypeProtocol::Room, id) << std::endl;
-	std::cout << "alive : " << offsetof(RtypeProtocol::Room, alive) << std::endl;
-	std::cout << "connected_user : " << offsetof(RtypeProtocol::Room, connected_users) << std::endl;
-	std::cout << "nb_connected_users : " << offsetof(RtypeProtocol::Room, nb_connected_users) << std::endl;
-	std::cout << "ipassword_flagd : " << offsetof(RtypeProtocol::Room, password_flag) << std::endl;
-	std::cout << "pass_md5 : " << offsetof(RtypeProtocol::Room, pass_md5) << std::endl;
-	std::cout << "room_name : " << offsetof(RtypeProtocol::Room, room_name) << std::endl;
-	std::cout << "ready_users : " << offsetof(RtypeProtocol::Room, ready_users) << std::endl;
-	std::cout << "locked : " << offsetof(RtypeProtocol::Room, locked) << std::endl;
-	std::cout << "users_id : " << offsetof(RtypeProtocol::Room, users_id) << std::endl;
+  RtypeProtocol::Room	room;
+  std::cout << "id : " << offsetof(RtypeProtocol::Room, id) << std::endl;
+  std::cout << "alive : " << offsetof(RtypeProtocol::Room, alive) << std::endl;
+  std::cout << "connected_user : " << offsetof(RtypeProtocol::Room, connected_users) << std::endl;
+  std::cout << "nb_connected_users : " << offsetof(RtypeProtocol::Room, nb_connected_users) << std::endl;
+  std::cout << "ipassword_flagd : " << offsetof(RtypeProtocol::Room, password_flag) << std::endl;
+  std::cout << "pass_md5 : " << offsetof(RtypeProtocol::Room, pass_md5) << std::endl;
+  std::cout << "room_name : " << offsetof(RtypeProtocol::Room, room_name) << std::endl;
+  std::cout << "ready_users : " << offsetof(RtypeProtocol::Room, ready_users) << std::endl;
+  std::cout << "locked : " << offsetof(RtypeProtocol::Room, locked) << std::endl;
+  std::cout << "users_id : " << offsetof(RtypeProtocol::Room, users_id) << std::endl;
   Network::Service	service;
   Application::Server	server(service);
 
