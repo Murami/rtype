7913777775b0ed61110af6aa2fc6571c8f44ab2c
senesi_t <senesi.theo@gmail.com>
2014-12-21 15:15:25 +0100

    test offset

--- new_network_rtype/src/main.cpp
+++ new_network_rtype/src/main.cpp
@@ -8,16 +8,16 @@
 int main()
 {
 	RtypeProtocol::Room	room;
-	std::cout << "id : " << sizeof(room.id) << std::endl;
-	std::cout << "alive : " << sizeof(room.alive) << std::endl;
-	std::cout << "connected_user : " << sizeof(room.connected_users) << std::endl;
-	std::cout << "nb_connected_users : " << sizeof(room.nb_connected_users) << std::endl;
-	std::cout << "ipassword_flagd : " << sizeof(room.password_flag) << std::endl;
-	std::cout << "pass_md5 : " << sizeof(room.pass_md5) << std::endl;
-	std::cout << "room_name : " << sizeof(room.room_name) << std::endl;
-	std::cout << "ready_users : " << sizeof(room.ready_users) << std::endl;
-	std::cout << "locked : " << sizeof(room.locked) << std::endl;
-	std::cout << "users_id : " << sizeof(room.users_id) << std::endl;
+	std::cout << "id : " << offsetof(RtypeProtocol::Room, id) << std::endl;
+	std::cout << "alive : " << offsetof(RtypeProtocol::Room, alive) << std::endl;
+	std::cout << "connected_user : " << offsetof(RtypeProtocol::Room, connected_users) << std::endl;
+	std::cout << "nb_connected_users : " << offsetof(RtypeProtocol::Room, nb_connected_users) << std::endl;
+	std::cout << "ipassword_flagd : " << offsetof(RtypeProtocol::Room, password_flag) << std::endl;
+	std::cout << "pass_md5 : " << offsetof(RtypeProtocol::Room, pass_md5) << std::endl;
+	std::cout << "room_name : " << offsetof(RtypeProtocol::Room, room_name) << std::endl;
+	std::cout << "ready_users : " << offsetof(RtypeProtocol::Room, ready_users) << std::endl;
+	std::cout << "locked : " << offsetof(RtypeProtocol::Room, locked) << std::endl;
+	std::cout << "users_id : " << offsetof(RtypeProtocol::Room, users_id) << std::endl;
   Network::Service	service;
   Application::Server	server(service);
 
