38f31c5346310f94472ae3c8ad6aea9cf4ed55b1
bichon_b <bichon_b@epitech.eu>
2014-12-23 18:09:52 +0100

    .

--- Client/src/RtypeClient.cpp
+++ Client/src/RtypeClient.cpp
@@ -218,7 +218,7 @@ void	RtypeClient::onPing(RtypeProtocol::PingPong)
 void	RtypeClient::onGameStart()
 {
   _menuView->stop();
-  //_gameView->run(*_window, &_mutex);
+  _gameView->run(*_window, &_mutex);
   std::cout << __FILE__ << ":" << __LINE__ << "\t" << __FUNCTION__ << std::endl;
 }
 
