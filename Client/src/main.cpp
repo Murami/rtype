2debe61fb8389ffd9077880568251d057d3662d0
senesi_t <senesi.theo@gmail.com>
2014-12-27 14:49:40 +0100

    debug client

--- Client/src/main.cpp
+++ Client/src/main.cpp
@@ -4,6 +4,9 @@
 
 int		main()
 {
+    std::cout << "Sizeof State : " << sizeof(RtypeProtocol::State) << std::endl;
+  std::cout << "id      " << offsetof (RtypeProtocol::State, id) << std::endl;
+  std::cout << "state : " << offsetof (RtypeProtocol::State, state) << std::endl;
   RtypeClient	client;
 
   try
